cbd66d19319046a82e34d9655a5eed58d73f566a
SouravMandal07 <souravmandal.sou07@gmail.com>
2024-04-12 11:32:02 +0530

    tried but failed responsieve

--- js/script.js
+++ js/script.js
@@ -15,13 +15,15 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () =>{
     sections.forEach(sec => {
-        let top = window.screenY;
+        // let top = window.screenY;
+        let top = window.scrollY;
+
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
-            navLinks.forEach.apply(links => {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
